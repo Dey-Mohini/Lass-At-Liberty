58f4dcaa34fccc8a742b3a42ea48c3b2555c35e7
1804351 <you@example.com>
2021-01-05 21:42:35 +0530

    Dashboard to home page after login updated

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 const Blog = require('../models/Blog')
-
-
 router.get('/', ensureGuest, (req, res) => {
   res.render('../views/layouts/home', {
     layout: 'home',
+  })
+})
+
+router.get('/home', ensureAuth, (req, res) => {
+  res.render('../views/layouts/homelogin', {
+     layout: 'homelogin',
   })
 })
 router.get('/login', ensureGuest, (req, res) => {
